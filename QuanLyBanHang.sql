5cae028de2d8dffe2d19e287a7bc949c7f15fd89
pnson1322 <pnson22@gmail.com>
2024-09-20 12:59:52 +0700

    Change

--- QuanLyBanHang.sql
+++ QuanLyBanHang.sql
@@ -1,7 +1,9 @@
+﻿-- Tạo database quản lí bán hàng
 CREATE DATABASE QUANLIBANHANG 
 
 USE QUANLIBANHANG
 
+-- Tạo các bảng của database
 CREATE TABLE KHACHHANG(
 	MAKH char(4) PRIMARY KEY,
 	HOTEN varchar(40),
@@ -42,18 +44,36 @@ CREATE TABLE CTHD(
 	PRIMARY KEY (SOHD, MASP),
 );
 
+-- 2.	Thêm vào thuộc tính GHICHU có kiểu dữ liệu varchar(20) cho quan hệ SANPHAM.
 ALTER TABLE SANPHAM
 ADD GHICHU VARCHAR(20)
 
+-- 3.	Thêm vào thuộc tính LOAIKH có kiểu dữ liệu là tinyint cho quan hệ KHACHHANG.
 ALTER TABLE KHACHHANG
 ADD LOAIKH TINYINT
 
+-- 4.	Sửa kiểu dữ liệu của thuộc tính GHICHU trong quan hệ SANPHAM thành varchar(100).
 ALTER TABLE SANPHAM
 ALTER COLUMN GHICHU VARCHAR(100)
 
+-- 5.	Xóa thuộc tính GHICHU trong quan hệ SANPHAM.
 ALTER TABLE SANPHAM
 DROP COLUMN GHICHU
 
-ALTER TABLE SANPHAM ADD
-CONSTRAINT CHECK (GIA > 500)
+-- 6.	Làm thế nào để thuộc tính LOAIKH trong quan hệ KHACHHANG có thể lưu các giá trị là: “Vang lai”, “Thuong xuyen”, “Vip”, …
+ALTER TABLE KHACHHANG
+ALTER COLUMN LOAIKH VARCHAR(20)
 
+-- 7.	Đơn vị tính của sản phẩm chỉ có thể là (“cay”,”hop”,”cai”,”quyen”,”chuc”)
+ALTER TABLE SANPHAM ADD 
+CONSTRAINT CK_DVT CHECK(DVT in ('cay', 'hop', 'cai', 'quyen', 'chuc'))
+
+-- 8.	Giá bán của sản phẩm từ 500 đồng trở lên.
+ALTER TABLE SANPHAM ADD
+CONSTRAINT CK_GIA CHECK (GIA > 500)
+
+-- 9.	Mỗi lần mua hàng, khách hàng phải mua ít nhất 1 sản phẩm. Đã thêm sẵn trên quan hệ.
+
+-- 10.	Ngày khách hàng đăng ký là khách hàng thành viên phải lớn hơn ngày sinh của người đó.
+ALTER TABLE KHACHHANG ADD 
+CONSTRAINT CK_NGDK CHECK(NGDK > NGSINH)
